9d0034dd45a704fd724730626857efc71a82a397
alberto <alberto056699@gmail.com>
2021-05-18 15:13:25 -0400

    second commit

--- public/js/user-list-public.js
+++
@@ -1,9 +0,0 @@
-(function( $ ) {
-	'use strict';
-
-	fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-		
-
-})( jQuery );
